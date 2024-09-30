15f6e1d3a97c33df0073ae16afbc48a0498db339
PRANTA Dutta <pranta.dutta@kdsgroup.net>
2024-09-30 16:53:26 +0600

    added kubernetes version to be 1.31

--- pkg/installer/install_kubernetes.go
+++ pkg/installer/install_kubernetes.go
@@ -17,16 +17,16 @@ func InstallKubernetes() {
 
 	// Download the public signing key for the Kubernetes package repositories
 	utils.RunCommand(`sudo mkdir -p -m 755 /etc/apt/keyrings`)
-	utils.RunCommand(`curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg`)
+	utils.RunCommand(`curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg`)
 	utils.RunCommand(`sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg`)
 
 	// Add the Kubernetes apt repository
-	utils.RunCommand(`echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list`)
+	utils.RunCommand(`echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list`)
 	utils.RunCommand(`sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list`)
 
 	// Update apt package index and install Kubernetes components
 	utils.RunCommand(`sudo apt-get update`)
-	utils.RunCommand(`sudo apt-get install -y kubelet kubeadm kubectl`)
+	utils.RunCommand(`sudo apt-get install -y kubelet=1.30.1-00 kubeadm=1.30.1-00 kubectl=1.30.1-00`)
 	utils.RunCommand(`sudo apt-mark hold kubelet kubeadm kubectl`)
 
 	log.Success("Successfully installed Kubernetes with kubelet, kubeadm and kubectl in the system")
